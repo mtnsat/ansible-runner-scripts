65eba6794e63fd3f9b0bf781a1aba568514e5fc6
Shaun Smiley <senorsmile@gmail.com>
2017-10-20 16:54:49 -0700

    reflect site.yml as default playbook

--- base_run.sh
+++ base_run.sh
@@ -29,7 +29,7 @@
 #   source base_run.sh
 #
 #   export ANSIBLE_BASE_RUN_MODE='playbook'
-#   export ANSIBLE_PLAYBOOK='playbook'
+#   export ANSIBLE_PLAYBOOK='site.yml'
 #   ./base_run.sh "${@}"
 #
 #----------------------------------------------------
