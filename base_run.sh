cafd826d10da4fbe3e1f2380384f8ce3c01d132b
Shaun Smiley <senorsmile@gmail.com>
2017-10-20 16:53:53 -0700

    add base_run.sh, use_ansible.sh and requirements

---
+++ base_run.sh
@@ -0,0 +1,105 @@
+#!/usr/bin/env bash
+#----------------------------------------------------
+#
+# base_run.sh - base script to be run by run_*
+#
+# ENV Vars:
+#   VAGRANT_MODE - [0,1] 
+#     - to be used with bovine-inventory's vagrant mode
+#   ANSIBLE_BASE_RUN_MODE - ["playbook","ad-hoc"]
+#     - specify which mode to run ansible in
+#   ANSIBLE_PLAYBOOK - defaults to "site.yml"
+#     - specify playbook to pass to ansible-playbook
+#     - NB: only used when run mode is "playbook"
+#   ANSIBLE_BASE_ARA - ["0","1"]
+#     - a bash STRING (not numeral) to enable ARA
+#   VAULT_PASSWORD_FILE - 
+#
+# CONFIGURATION:
+#   Configuration can be overridden by defining vars
+#   in the following script, which is not git tracked:
+#     - cfg.base_run.sh
+#   A sample is provided in
+#     - cfg.base_run.sh.example
+# 
+#
+# Example:
+#   --- run_this.sh ---
+#   #!/usr/bin/env bash
+#   source base_run.sh
+#
+#   export ANSIBLE_BASE_RUN_MODE='playbook'
+#   export ANSIBLE_PLAYBOOK='playbook'
+#   ./base_run.sh "${@}"
+#
+#----------------------------------------------------
+#set -e
+
+if [[ -f cfg.base_run.sh ]]; then
+  source cfg.base_run.sh
+fi
+
+export ANSIBLE_FORCE_COLOR="${ANSIBLE_FORCE_COLOR:-true}"
+export VAGRANT_MODE="${VAGRANT_MODE:-0}" #1=enabled, disabled by default
+
+INOPTS=("$@")
+
+if [ $# -eq 0 ]; then
+  echo "No arguments provided, please provide at least one argument"
+  echo "Example: ./run_this.sh -v"
+  exit 1
+fi
+
+mode="${ANSIBLE_BASE_RUN_MODE:-playbook}"
+echo "****** RUN MODE= ${ANSIBLE_BASE_RUN_MODE}"
+
+if [[ ${ANSIBLE_BASE_RUN_MODE} == 'playbook' ]]; then
+  playbook="${ANSIBLE_PLAYBOOK:-site.yml}"
+  echo "****** PLAYBOOK= ${playbook}"
+fi
+
+echo "****** VAGRANT_MODE= ${VAGRANT_MODE}"
+
+
+source use_ansible.sh
+
+# Plaintext vault decryption key, not checked into SCM
+if [[ -f $VAULT_PASSWORD_FILE ]]; then
+  VAULTOPTS="--vault-password-file=$VAULT_PASSWORD_FILE"
+else
+  if [[ $ANSIBLE_BASE_RUN_MODE == "playbook" ]]; then
+    # tag plays and role with "required_vault" to be able to skip vault required tasks
+    VAULTOPTS="--skip-tags requires_vault"
+  else
+    # there is no --skip-tags for ad-hoc mode
+    VAULTOPTS=""
+  fi
+fi
+
+setup_ara() {
+  export ara_location=$(python -c "import os,ara; print(os.path.dirname(ara.__file__))")
+  export ANSIBLE_CALLBACK_PLUGINS=$ara_location/plugins/callbacks
+  export ANSIBLE_ACTION_PLUGINS=$ara_location/plugins/actions
+  export ANSIBLE_LIBRARY=$ara_location/plugins/modules
+}
+
+run_ansible() {
+  if [[ ANSIBLE_BASE_ARA == '1' ]]; then
+    setup_ara
+  fi
+
+  if [[ ${ANSIBLE_BASE_RUN_MODE} == 'playbook' ]]; then
+    ansible-playbook --diff "${playbook}" $VAULTOPTS "${INOPTS[@]}"
+  elif [[ ${ANSIBLE_BASE_RUN_MODE} == 'ad-hoc' ]]; then
+    ansible --diff "${INOPTS[@]}" $VAULTOPTS
+  else
+    echo "Invalid run mode: ${ANSIBLE_BASE_RUN_MODE}"
+    exit 15
+  fi
+}
+
+time run_ansible
+retcode=$?
+
+
+exit $retcode
---
+++ use_ansible.sh
@@ -0,0 +1,200 @@
+#!/usr/bin/env bash
+
+#-------------------------------------
+# use_ansible.sh
+#
+# Script to install and run ansible 2.x from virtualenv
+# or from github source checked out in /tmp
+#
+# ( This only works from within a script.
+#   if you source it from the command prompt,
+#   nothing will happen. )
+#
+# Authors: Shaun Smiley
+#          Rowin Andruscavage
+#
+# VENV vars
+#   USE_ANSIBLE_MODE=virtualenv ## default
+#   USE_ANSIBLE_VENV_VER=2.4 # example value
+#
+# GIT vars
+#   USE_ANSIBLE_MODE=git
+#   USE_ANSIBLE_VER='v2.4.0.0-1' # example value
+#
+#-------------------------------------
+
+#-------------------------------------
+##- vars
+#-------------------------------------
+## virtualenv or git
+run_mode="${USE_ANSIBLE_MODE:-virtualenv}"
+
+## virtualenv configuration
+[[ -n $USE_ANSIBLE_VENV_DIR && -n $USE_ANSIBLE_VENV_REQ ]] && {
+  VENV="${USE_ANSIBLE_VENV_DIR:-venv-ansible2.0}"
+  REQUIREMENTS="${USE_ANSIBLE_VENV_REQ:-requirements-ansible2.0.txt}"
+} || {
+  [[ -n $USE_ANSIBLE_VENV_VER ]] || {
+    USE_ANSIBLE_VENV_VER='2.4' #default
+  }
+
+  case $USE_ANSIBLE_VENV_VER in
+    2.0)
+      VENV='venv-ansible2.0'
+      REQUIREMENTS='requirements-ansible2.0.txt'
+      ;;
+    2.2)
+      VENV='venv-ansible2.2'
+      REQUIREMENTS='requirements-ansible2.2.txt'
+      ;;
+    2.3)
+      VENV='venv-ansible2.3'
+      REQUIREMENTS='requirements-ansible2.3.txt'
+      ;;
+    2.4)
+      VENV='venv-ansible2.4'
+      REQUIREMENTS='requirements-ansible2.4.txt'
+      ;;
+    *)
+      echo 'unrecognized USE_ANSIBLE_VENV_VER'
+      echo 'use "2.0", "2.2", "2,3" or "2.4"'
+      exit 1
+      ;;
+  esac
+}
+
+## git checkout dir
+co='/usr/local/bin/ansible-git'
+co_run="${co}/hacking/env-setup"
+
+## debug mode
+debug_mode="${USE_ANSIBLE_DEBUG:-false}"
+[[ $debug_mode == 'true' ]] && { echo "run_mode: ${run_mode}"; echo; }
+
+## git checkout tag
+# ansible_ver="${USE_ANSIBLE_VER:-v2.0.2.0-1}"
+# ansible_ver="${USE_ANSIBLE_VER:-v2.1.6.0-1}"
+# ansible_ver="${USE_ANSIBLE_VER:-v2.2.3.0-1}"
+ansible_ver="${USE_ANSIBLE_VER:-v2.3.2.0-1}" # default for git method
+# ansible_ver="${USE_ANSIBLE_VER:-v2.4.0.0-1}"
+# ansible_ver="${USE_ANSIBLE_VER:-v2.4.1.0-0.4.rc2}"
+
+
+
+
+#-------------------------------------
+##- virtualenv functions
+#-------------------------------------
+virtualenv_setup() {
+  ## One-time OS setup for virtualenv
+
+  ## Ubuntu virtualenv install
+  [[ $(which apt-get) ]] && {
+    [[ $(which virtualenv) ]] || {
+      sudo apt-get install -y python-virtualenv
+    }
+    for PKG in python-dev libffi-dev libssl-dev build-essential ; do
+      [[ $(dpkg -s $PKG) ]] || {
+        sudo apt-get install -y -f $PKG
+      }
+    done
+  }
+
+  ## Mac OS X virtualenv install
+  [[ $(uname) == 'Darwin' ]] && {
+    [[ $(which virtualenv) ]] || {
+      brew install pyenv-virtualenv
+    }
+  }
+}
+
+run_from_virtualenv() {
+  if [[ ! -f $REQUIREMENTS ]]; then
+    echo "Requirements file ${REQUIREMENTS} missing."
+    echo "Exiting."
+    exit 1
+  fi
+  [ -d $VENV ] || {
+    virtualenv $VENV
+    source ./$VENV/bin/activate
+    pip install -U setuptools
+    pip install -U pip
+    pip install -r $REQUIREMENTS
+    deactivate
+  }
+
+  echo "Sourcing ansible venv."
+  source ./$VENV/bin/activate
+  echo "Ensuring python requirements."
+  [[ $debug_mode == 'true' ]] && {
+    pip install -r $REQUIREMENTS
+  } || {
+    pip install -r $REQUIREMENTS >/dev/null
+  }
+
+  echo; ansible --version; echo
+}
+
+
+
+
+#-------------------------------------
+##- git functions
+#-------------------------------------
+co_version() {
+  ## checkout desired version of ansible from git
+  cd "${co}" >/dev/null 2>&1
+
+  local current_version=$(git describe --tags)
+  [[ "$current_version" == "$ansible_ver" ]] && {
+    echo "Already on ansible $ansible_ver."
+  } || {
+    echo "Updating ansible git..."
+    git checkout devel >/dev/null 2>&1
+    git pull --rebase >/dev/null 2>&1
+    git checkout "${ansible_ver}" >/dev/null 2>&1
+    git submodule update --init --recursive >/dev/null 2>&1
+    #verify what tag we're on now
+    echo "Now on Ansible $(git describe --tags)"
+  }
+
+}
+
+source_ansible() {
+  ## source ansible version into current shell
+  source "${co_run}" >/dev/null 2>&1
+}
+
+run_from_git() {
+  [[ $(which git) ]] || {
+      echo "Git not installed.  Aborting..."
+      exit 1
+  }
+
+  [[ -f "${co_run}" ]] || {
+      echo "Ansible git repo not yet checked out at ${co}."
+      sudo mkdir "${co}"
+      sudo chown $(whoami) "${co}"
+      git clone https://github.com/ansible/ansible.git "${co}"
+  }
+
+  ## save current directory
+  pushd .  >/dev/null 2>&1
+
+  co_version
+  source_ansible
+
+  ## return to original directory
+  popd  >/dev/null 2>&1
+
+  echo; ansible --version; echo
+}
+
+
+
+
+#-------------------------------------
+##- main
+#-------------------------------------
+[[ $run_mode == 'virtualenv' ]] && run_from_virtualenv
+[[ $run_mode == 'git' ]] && run_from_git
