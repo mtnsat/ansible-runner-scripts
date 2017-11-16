b8ac9c25b8129118ce6bf5981202f03612aa259d
Shaun Smiley <senorsmile@gmail.com>
2017-11-16 14:21:54 -0800

    comment - must export envvars BEFORE sourcing

--- base_run.sh
+++ base_run.sh
@@ -26,10 +26,12 @@
 # Example:
 #   --- run_this.sh ---
 #   #!/usr/bin/env bash
-#   source base_run.sh
 #
 #   export ANSIBLE_BASE_RUN_MODE='playbook'
 #   export ANSIBLE_PLAYBOOK='site.yml'
+#
+#   source base_run.sh
+
 #   ./base_run.sh "${@}"
 #
 #----------------------------------------------------
